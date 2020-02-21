a745d29f6af6c7224d41b43e07c6fa006c8a60f1
Obi <cnnaeto@andrew.cmu.edu>
2020-02-21 00:32:30 -0500

    Add selfie image to main page and add custome quotes from fav tv shows

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Best Lawyer ever', 'Its Gonna be legendary', 'The North remembers', 'The kevlar of knowing the answer'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
